6515c4623b308a2027339f27479ae2ebfd859b7a
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-06-26 19:38:14 +0100

    Two functions created to create starships collection and insert ship documents from the starships api

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Data 22 Star Wars Project (Josh Blackmore)
 
-
 ## Usage
 
 
 ## Testing
 Use 'pytest -v' on the parent directory to view the tests conducted during the TDD (Test Driven Development) part of this small project.
+
+
+## Research
+- PyMongo: https://www.w3schools.com/python/python_mongodb_getstarted.asp
+- PyMongo Docs: https://pymongo.readthedocs.io/en/stable/
 
 
 ## Other Information and Links
--- starwars/app/main.py
+++ starwars/app/main.py
@@ -5,5 +5,30 @@ from pprint import pprint
 get_api = requests.get("https://swapi.dev/api/")
 
 client = pymongo.MongoClient()
-db = client['starwars']
+sw_db = client['starwars']
 
+
+def create_starships_collection():
+    # Create the new updated starships collection
+    if "starships" not in sw_db.list_collection_names():
+        sw_db["starships"]
+        print("Starships collection has been created.")
+    else:
+        # Drop the collection so that it is remade in the next stage, this avoids data duplication
+        sw_db["starships"].drop()
+        print("Starships collection already exists.")
+
+
+def insert_starships():
+
+    starships_json = requests.get("https://swapi.dev/api/starships/").json()
+
+    for i in starships_json["results"]:
+        ss_name = i["name"]
+        sw_db.starships.insert_one(i)
+        print(f"Inserted document for {ss_name}")
+
+
+if __name__ == "__main__":
+    create_starships_collection()
+    insert_starships()
