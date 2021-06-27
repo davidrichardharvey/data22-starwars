c52151c06225424e462d4ac7780398d08a59c05d
DaveCLilley <lilleyd@lilleyc.karoo.co.uk>
2021-06-27 13:51:53 +0100

    db drop test

--- starwars/app/api_request.py
+++ starwars/app/api_request.py
@@ -1,6 +1,9 @@
 import requests
+from pprint import pprint
+import json
 
 
 def api_request():
     starships = requests.get("https://swapi.dev/api/starships")
-    return starships.status_code
+    return starships
+
---
+++ starwars/tests/test_db_drop.py
@@ -0,0 +1,6 @@
+from starwars.app.db_drop import db_drop
+import pymongo
+
+
+def test_db_drop
+    assert db.starships.find_many() == {}
