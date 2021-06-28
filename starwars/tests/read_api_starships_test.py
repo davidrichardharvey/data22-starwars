223c622e00c4cdd885d397aaa48b669dd47180ad
milan-crypto <85555923+milan-crypto@users.noreply.github.com>
2021-06-28 22:22:49 +0100

    Update

--- starwars/tests/read_api_starships_test.py
+++ starwars/tests/read_api_starships_test.py
@@ -1,8 +1,10 @@
 from starwars.app.read_api_starships import *
 
 
-# Test that checks if the function is printing the correct info from the API
-def test_read_api():
+# Test that checks if the function is printing the correct info from the API.
+
+
+def test_read_api_starships():
     assert str(type(read_api_page_one())) == "<class 'dict'>"
     assert str(type(read_api_page_two())) == "<class 'dict'>"
     assert str(type(read_api_page_three())) == "<class 'dict'>"
