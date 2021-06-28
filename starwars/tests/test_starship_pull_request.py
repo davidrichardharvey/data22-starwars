796c263466c515a20de640cbcc8efecf9337044f
AFRothwell <ARothwell@spartaglobal.com>
2021-06-28 10:35:36 +0100

    Completed User Story 4. All files now meeting PEP-8 Standards.

--- starwars/tests/test_starship_pull_request.py
+++ starwars/tests/test_starship_pull_request.py
@@ -5,14 +5,16 @@ Created on Sun Jun 27 18:14:05 2021
 
 @author: Andrew Rothwell
 """
-'''
-
-Tests that the "status_code" function in the "starship_pull_request.py" file
-is correctly returning the status_code "200"
-
-'''
 
 
 def test_starship_pull_request():
 
+    # Tests that the "status_code" function in the "starship_pull_request.py"
+    # file is correctly returning the status_code "200"
+
     assert status_code() == 200
+
+# Test that the starship pull request has pulled all the correct data by
+# checking if the list of dictionaries is 36 values in length.
+
+    assert len(starship_data) == 36
