1f42dd325bb4fc114f8ec4a967c26dc312a7b6f3
milan-crypto <85555923+milan-crypto@users.noreply.github.com>
2021-06-28 09:50:47 +0100

    Update

--- star_wars_starships_update.py
+++ star_wars_starships_update.py
@@ -5,5 +5,5 @@ import requests
 response = requests.get("https://swapi.dev/api/starships")
 pprint(response.json())
 
-# client = pymongo.MongoClient()
-# db = client['starwars']
+client = pymongo.MongoClient()
+db = client['starwars']
