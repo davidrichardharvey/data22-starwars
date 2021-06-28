730c422bbadbebefdb59797902add440288d5b32
Amy <amy.zhang7@gmail.com>
2021-06-28 14:15:37 +0100

    test get_all_people()

--- starwars/tests/test_api.py
+++ starwars/tests/test_api.py
@@ -1,4 +1,6 @@
 from starwars.app.api_request import *
+from starwars.app.sw_mongo import *
+import pymongo
 
 
 def test_api_call():
@@ -11,3 +13,11 @@ def test_get_starships():
 
 def test_get_all_pages_starships():
     assert len(get_all_pages_starships()) == 36
+
+
+def test_get_all_people():
+    assert len(get_all_people()) == 81
+
+
+def test_create_starships_collection():
+    assert starships_coll.find() != None
