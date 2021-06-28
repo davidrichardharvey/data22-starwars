39bc3fb5440874b9be2571cb6b6569df034738dc
Amy <amy.zhang7@gmail.com>
2021-06-28 10:23:07 +0100

    get and test status code

--- starwars/app/api_request.py
+++ starwars/app/api_request.py
@@ -1,6 +1,8 @@
 import requests
+import json
+from pprint import pprint
+
 
 def api_call():
-    return requests.get("https://swapi.dev/api/")
-
-print(api_call())
+    starships_api = requests.get("https://swapi.dev/api/starships/")
+    return starships_api.status_code
--- starwars/tests/test_api.py
+++ starwars/tests/test_api.py
@@ -1,4 +1,5 @@
 from starwars.app.api_request import api_call
 
+
 def test_api_call():
-    assert str(api_call()) == '<Response [200]>'
+    assert api_call() == 200
