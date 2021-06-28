4bc1b1511a4684ddb1fb1f2b0dd0664468cb7623
Amy <amy.zhang7@gmail.com>
2021-06-28 10:30:46 +0100

    test get_starships() passed

--- starwars/app/api_request.py
+++ starwars/app/api_request.py
@@ -6,3 +6,10 @@ from pprint import pprint
 def api_call():
     starships_api = requests.get("https://swapi.dev/api/starships/")
     return starships_api.status_code
+
+
+def get_starships():
+    headers = {'Content-Type': 'application/json'}
+    response = requests.get("https://swapi.dev/api/starships/", headers=headers)
+    response_json = response.json()
+    return response_json
--- starwars/tests/test_api.py
+++ starwars/tests/test_api.py
@@ -1,5 +1,9 @@
-from starwars.app.api_request import api_call
+from starwars.app.api_request import *
 
 
 def test_api_call():
     assert api_call() == 200
+
+
+def test_get_starships():
+    assert get_starships()['count'] == 36
