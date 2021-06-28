efd0adfc79fbaec3423a4892e097bdb30c349c75
MattBrackenbury <matt.a.brackenbury@gmail.com>
2021-06-28 15:44:24 +0100

    Working Code Commit

--- starwars/app/data_update.py
+++ starwars/app/data_update.py
@@ -21,31 +21,32 @@ def pilot_api_read(pilot_api):
     return requests.get(pilot_api)
 
 
-pilot_character_ordered = []  # ordered list of the pilots, will match original
+
 
 
 # creates an ordered list of the pilot character data. Calls the api url.
 def create_pilot_character_list(pilots):
+    pilot_character_ordered = []  # ordered list of the pilots, will match original
     for crew in pilots:
         if not crew:  # if no crew
-            pilot_character_ordered.append('Empty')  # adds empty list
+            pilot_character_ordered.append(crew)  # adds empty list
         else:  # if crew
             starship_pilots = []  # creates empty list for each ship
             for pilot in crew:  # allows for multiple pilots per ship
                 starship_pilots.append(pilot_api_read(pilot).json())  # adds each pilot character to its ship list
             pilot_character_ordered.append(starship_pilots)  # adds total ship to ordered list
+    return pilot_character_ordered
 
 
 # updates the list of starship pilots urls with the character lists.
-def update_starships(starships):
+def update_starships(starships, pilot_character_ordered):
     for ship in starships:
-        print('Pilot_char_orderd:', pilot_character_ordered[0])
         ship['pilots'] = pilot_character_ordered[0]
         pilot_character_ordered.pop(0)
 
 def run_all():
     starships = read_starships_api()
     pilots = create_pilot_list(starships)
-    create_pilot_character_list(pilots)
-    update_starships(starships)
+    pilot_character_ordered = create_pilot_character_list(pilots)
+    update_starships(starships, pilot_character_ordered)
     return starships
