3cd4d63c4860720c4299c592d11d536423d23e90
Matt Brackenbury <84974682+MattBrackenbury@users.noreply.github.com>
2021-06-29 09:03:53 +0100

    Update test_data_update.py

--- starwars/tests/test_data_update.py
+++ starwars/tests/test_data_update.py
@@ -5,6 +5,7 @@ def test_read_starships_api_type(): # tests if returns the correct type of data
 
 def test_create_pilots_list():
     assert str(type(create_pilot_list())) == "<class 'list'>"
-
+    
 def test_pilot_api_read():
-    assert str(type(pilot_api_read())) == "<class 'requests.models.Response'>"
+    info = pilot_api_read('https://swapi.dev/api/people/4/')
+    assert info.status_code == 200
